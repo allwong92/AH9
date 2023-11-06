60b5b73c65c3269ad944a8cc8796cc9923423649
Wong <allison.wong@fiserv.com>
2023-11-06 15:11:07 -0500

    AH9 Submission

--- ECSource.java
+++ ECSource.java
@@ -10,8 +10,39 @@ class Node<T> {
 
 class ECSource {
   public static <T> boolean isUnivalueList(Node<T> head) {
-    // todo
-    return true;
+    /*
+     * This method takes in the head of a linked list and determines whether
+     * the linked list contains exactly one unique value. Returns a boolean.
+     */
+    boolean isUnique = true;    // boolean to return, assumes list contains one unique value
+    while (head != null && head.next != null) {   // while head and node next to head aren't null
+      if (!head.next.val.equals(head.val)){   // if next value != current value
+        isUnique = false;     // there cannot be one value in list if two aren't equal
+        break;   
+      }
+      head = head.next;     // traverse to next node
+    }
+    return isUnique;
+  }
+
+  public static <T> boolean isUnivalueListRecursive(Node<T> head) {
+    /*
+     * This method takes in the head of a linked list and determines whether
+     * the linked list contains exactly one unique value RECURSIVELY. 
+     * Returns a boolean.
+     */
+    boolean isUnique = true;    // boolean to return, assumes list contains one unique value
+
+    if (head.next != null){
+      if (!head.next.val.equals(head.val)){   // if next value != current value
+          isUnique = false;     // there cannot be one value in list if two aren't equal
+          return isUnique; 
+      }  
+      
+      isUnivalueListRecursive(head.next);
+
+    }
+    return isUnique;
   }
 
   public static void main(String[] args) {
@@ -20,5 +51,35 @@ class ECSource {
 
         // Printing solution
         System.out.println((ECSource.isUnivalueList(z)));
+
+        Node<Integer> u = new Node<>(2);
+        Node<Integer> v = new Node<>(2);
+        Node<Integer> w = new Node<>(2);
+        Node<Integer> x = new Node<>(2);
+        Node<Integer> y = new Node<>(2);
+
+        u.next = v;
+        v.next = w;
+        w.next = x;
+        x.next = y;
+
+        // 2 -> 2 -> 2 -> 2 -> 2
+
+        System.out.println(ECSource.isUnivalueListRecursive(u)); // true
+
+        Node<Integer> l = new Node<>(2);
+        Node<Integer> m = new Node<>(2);
+        Node<Integer> n = new Node<>(3);
+        Node<Integer> o = new Node<>(3);
+        Node<Integer> p = new Node<>(2);
+
+        l.next = m;
+        m.next = n;
+        n.next = o;
+        o.next = p;
+
+        // 2 -> 2 -> 3 -> 3 -> 2
+
+        System.out.println(ECSource.isUnivalueList(l)); // false
     }
 }
--- Source.java
+++ Source.java
@@ -10,8 +10,29 @@ class Node<T> {
   
   class Source {
     public static <T> Node<T> reverseList(Node<T> head) {
-      // todo
-      return head;
+      /*
+       * This method takes in the head of a linked list and reverses the
+       * order of the nodes in the linked list in place.
+       * 
+       * Returns the new head of the reversed linked list
+       */
+      
+      Node<T> prev_node = null;       // to have a node to point to previous
+      Node<T> next_node = null;       // to hold the next node
+      Node<T> current_node = head;    // to hold current node
+
+      while(current_node != null){  // while current node isn't null
+        next_node = current_node.next;    // make the next node the one after current node
+        current_node.next = prev_node;    // make one after current the previous (reverse)
+        prev_node = current_node;         // make the previous node the current
+        current_node = next_node;         // make current node the next node 
+      }
+
+      // to exit while loop, the current node has to be null. 
+      // The previous node is tail at end of loop (start of new list)
+      head = prev_node;   
+
+      return head; // return head of reversed list
     }
     
     public static void main(String[] args) {
@@ -19,8 +40,7 @@ class Node<T> {
         Node<String> y = new Node<>("y");
     
         x.next = y; // x -> y
-    
-        reverseList(x); // y -> x
+  
 
         // Printing solution
         Node<String> head = Source.reverseList(x);
@@ -28,6 +48,27 @@ class Node<T> {
         while (head != null) {
             System.out.println(head.val);
             head = head.next;            
+        }
+
+        Node<String> a = new Node<>("a");
+        Node<String> b = new Node<>("b");
+        Node<String> c = new Node<>("c");
+        Node<String> d = new Node<>("d");
+        Node<String> e = new Node<>("e");
+        Node<String> f = new Node<>("f");
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        // a -> b -> c -> d -> e -> f
+
+        Node<String> newHead = reverseList(a); // f -> e -> d -> c -> b -> a
+
+        while (newHead != null) {
+          System.out.println(newHead.val);
+          newHead = newHead.next;
         }
     }
   }
